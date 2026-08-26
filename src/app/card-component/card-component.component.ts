2934c4e9f4fae84859abbeb4f3aeecc6545b4c22
RyanFrag <92130127+RyanFrag@users.noreply.github.com>
2026-08-26 19:54:07 -0300

    add icons

--- src/app/card-component/card-component.component.ts
+++ src/app/card-component/card-component.component.ts
@@ -1,18 +1,27 @@
+import { openOutline } from 'ionicons/icons';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Carta } from '../models/carta';
 import { CommonModule, JsonPipe } from '@angular/common'
-
+import { IonicModule } from '@ionic/angular';
+import { ValorPipePipe } from '../pipes/valor-pipe-pipe';
+import { NaipePipePipe } from '../pipes/naipe-pipe-pipe';
+import { addIcons } from 'ionicons';
 @Component({
   selector: 'app-card-component',
   templateUrl: './card-component.component.html',
   styleUrls: ['./card-component.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule, ValorPipePipe, NaipePipePipe],
 })
 export class CardComponentComponent  implements OnInit {
 
   @Input() carta?: Carta;
   @Output() selecionar = new EventEmitter<Carta>();
-  constructor() { }
+  constructor() {
+    addIcons({
+      'open': openOutline,
+      'open-outline': openOutline
+    })
+   }
 
   ngOnInit() {}
 
